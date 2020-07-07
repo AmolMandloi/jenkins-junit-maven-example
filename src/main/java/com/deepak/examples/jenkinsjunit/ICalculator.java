6717b5f459af076bbeb2fbbfeb51ee92f0f76213
AmolMandloi <amolmandloi7@gmail.com>
2020-07-07 18:58:43 +0530

    added files

---
+++ src/main/java/com/deepak/examples/jenkinsjunit/ICalculator.java
@@ -0,0 +1,21 @@
+package com.deepak.examples.jenkinsjunit;
+
+public interface ICalculator {
+	int sum(int a, int b);
+
+	int subtraction(int a, int b);
+
+	int multiplication(int a, int b);
+
+	int divison(int a, int b) throws Exception;
+	
+	int square(int a);
+	
+	int cube(int c);
+	
+	int vol(int l, int b, int h); 
+	
+	double circle(int r);
+
+	boolean equalIntegers(int a, int b);
+}
